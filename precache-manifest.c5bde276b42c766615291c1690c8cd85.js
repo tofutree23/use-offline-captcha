6caecb726f737d5f5110b4965d20be7698d7c48f
Luke <tofutree23@gmail.com>
2022-05-13 13:46:29 +0900

    Updates

---
+++ precache-manifest.c5bde276b42c766615291c1690c8cd85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df97dc33f484be2599ff16fbda6b7a95",
+    "url": "/use-offline-captcha/index.html"
+  },
+  {
+    "revision": "8e52a5c1f2b6efc1ede9",
+    "url": "/use-offline-captcha/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "f76fc91a5530c30160ec",
+    "url": "/use-offline-captcha/static/js/2.afa04d52.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/use-offline-captcha/static/js/2.afa04d52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e52a5c1f2b6efc1ede9",
+    "url": "/use-offline-captcha/static/js/main.fbf1784c.chunk.js"
+  },
+  {
+    "revision": "8ee4b92630d061a67e05",
+    "url": "/use-offline-captcha/static/js/runtime-main.f4c23e46.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/use-offline-captcha/precache-manifest.c5bde276b42c766615291c1690c8cd85.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/use-offline-captcha/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
